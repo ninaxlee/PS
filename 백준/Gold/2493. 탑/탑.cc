1b8fda59861d118900ea9a477660bf94863925f4
Nina <109032458+ninaxlee@users.noreply.github.com>
2026-02-07 23:51:58 +0900

    [Gold V] Title: 탑, Time: 80 ms, Memory: 6300 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.cc
+++ 백준/Gold/2493. 탑/탑.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n, height;
-	cin >> n;
-	stack<pair<int, int>> tower;
-	tower.push({ 100000001, 0 }); //수신할 탑이 없는 경우
-	for (int i = 1; i <= n; i++) {
-		cin >> height;
-		while (height > tower.top().first) {
-			tower.pop();
-		}
-		cout << tower.top().second << " ";
-		tower.push({ height, i });
-	}
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, num;
+    cin>>n;
+    stack<pair<int, int>> s;
+    s.push({100000001, 0});
+    
+    for(int i=1;i<=n;i++){
+        cin>>num;
+        while(s.top().first < num){
+            s.pop();
+        }
+        cout<<s.top().second<<" ";
+        s.push({num, i});
+    }
 }
