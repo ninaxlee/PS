c491792b2cd44bce69f0d590f7f68d50d6dde817
Nina <109032458+ninaxlee@users.noreply.github.com>
2026-02-07 23:52:34 +0900

    [Silver II] Title: 스택 수열, Time: 12 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -2,35 +2,34 @@
 using namespace std;
 
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	int n, num, idx = 1;
-	stack<int> s;
-	vector<char> v;
-	bool correct = true;
-	cin >> n;
-	while (n > 0) {
-		cin >> num;
-
-		while (idx <= num) {
-			s.push(idx++);
-			v.push_back('+');
-		}
-
-		if (s.top() != num) {
-			correct = false;
-		}
-		s.pop();
-		v.push_back('-');
-		n--;
-
-	}
-	if (correct == true) {
-		for (auto x : v)
-			cout << x << '\n';
-	}
-	else
-		cout << "NO";
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int n, num, cnt = 1;
+    cin>>n;
+    
+    stack<int> s;
+    queue<char> q;
+    
+    while(n--) {
+        cin>>num;
+        
+        while(cnt<=num){
+            s.push(cnt++);
+            q.push('+');
+        }
+        
+        if(s.top() != num){
+            cout<<"NO";
+            return 0;
+        }
+        
+        s.pop();
+        q.push('-');
+    }
+    
+    while(!q.empty()){
+        cout<<q.front()<<'\n';
+        q.pop();
+    }
 }
