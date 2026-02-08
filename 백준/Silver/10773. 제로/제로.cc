2c082c913d1b1ad3fb46c06e08da012fb25dd765
Nina <109032458+ninaxlee@users.noreply.github.com>
2026-02-08 23:55:25 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -1,31 +1,24 @@
-#include<iostream>
-#include <stack>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-int main(int argc, char** argv)
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	stack <int> s;
-	int K;
-	int n;
-	cin >> K;
-	for (int i = 0; i < K; i++) {
-		cin >> n;
-		if (n == 0) {
-			s.pop();
-		}
-		else
-			s.push(n);
-	}
-	int sum = 0;
-	while (s.size() != 0) {
-		sum += s.top();
-		s.pop();
-	}
-	cout << sum;
-
-
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    int k, num, ret = 0;
+    stack<int> s;
+    cin>>k;
+    
+    for(int i=0;i<k;i++){
+        cin>>num;
+        if(num==0) s.pop();
+        else s.push(num);
+    }
+    
+    while(!s.empty()){
+        ret += s.top();
+        s.pop();
+    }
+    
+    cout<<ret;
 }
