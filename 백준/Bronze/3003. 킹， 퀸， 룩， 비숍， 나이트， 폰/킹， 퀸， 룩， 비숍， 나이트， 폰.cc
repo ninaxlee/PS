e26cccc52f5e14a91b88884895360030de637cd3
Nina <109032458+ninaxlee@users.noreply.github.com>
2026-02-28 23:29:39 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int k, q, r, b, kn, p;
+    cin>>k>>q>>r>>b>>kn>>p;
+    
+    cout<<-(k-1)<<" "<<-q+1<<" "<<-r+2<<" "<<-b+2<<" "<<-kn+2<<" "<<-p+8;
+}
